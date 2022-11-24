1bbe0db9dd7cf0d18d263599918a77446e63b94b
21218367 <eloliuliu9@gmail.com>
2022-11-24 11:54:21 +0100

    .

---
+++ src/devoirTD8/LancerDes.java
@@ -0,0 +1,15 @@
+package devoirTD8;
+
+public class LancerDes extends Aleat {
+		
+	public LancerDes() {
+	super(6);}
+		
+	public TroisDes lancer() {
+		TroisDes tire = new TroisDes(super.get(), super.get(), super.get());
+		return tire;
+			
+		}
+
+	}
+
